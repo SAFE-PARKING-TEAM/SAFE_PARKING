6f558a310a63029ce9b73fc7e37f70c8eff2ab88
hectorhorna <u202113453@upc.edu.pe>
2023-09-06 00:56:15 -0500

    Creacion de las clases Interface IEstacionamientoRepository, IEstacionamientoService, ILocalizacionRepository, ILocalizacionService. Además de la creacion de las clases Java EstacionamientoController, LocalizacionController, EstacionamientoServiceImplement, LocalizacionServiceImplement

---
+++ src/main/java/pe/edu/upc/aaw/safeparking/controllers/EstacionamientoController.java
@@ -0,0 +1,33 @@
+package pe.edu.upc.aaw.safeparking.controllers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.aaw.safeparking.dtos.EstacionamientoDTO;
+import pe.edu.upc.aaw.safeparking.entities.Estacionamiento;
+import pe.edu.upc.aaw.safeparking.serviceinterfaces.IEstacionamientoService;
+
+
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/estacionamiento")
+public class EstacionamientoController {
+    @Autowired
+    private IEstacionamientoService eS;
+    @PostMapping
+    public void registrar(@RequestBody EstacionamientoDTO dto){
+        ModelMapper m=new ModelMapper();
+        Estacionamiento d=m.map(dto,Estacionamiento.class);
+        eS.insert(d);
+    }
+    @GetMapping
+    public List<EstacionamientoDTO> listar(){
+        return eS.list().stream().map(x->{
+            ModelMapper m=new ModelMapper();
+            return m.map(x,EstacionamientoDTO.class);
+        }).collect(Collectors.toList());
+    }
+}
