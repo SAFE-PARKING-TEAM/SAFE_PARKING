2b24a44a4dbe13566d33db371737c1ea1fe55380
EliasJMZegarra <105391465+EliasJMZegarra@users.noreply.github.com>
2023-09-05 23:47:46 -0500

    Creacion de los paquetes Repositories, Controllers, ServiceImplements y ServiceInterface

---
+++ src/main/java/pe/edu/upc/aaw/safeparking/controllers/MembresiaController.java
@@ -0,0 +1,31 @@
+package pe.edu.upc.aaw.safeparking.controllers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.aaw.safeparking.dtos.MembresiaDTO;
+import pe.edu.upc.aaw.safeparking.entities.Membresia;
+import pe.edu.upc.aaw.safeparking.serviceinterfaces.IMembresiaService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/membresia")
+public class MembresiaController {
+    @Autowired
+    private IMembresiaService dS;
+    @PostMapping
+    public void registrar(@RequestBody MembresiaDTO dto){
+        ModelMapper m=new ModelMapper();
+        Membresia d=m.map(dto,Membresia.class);
+        dS.insert(d);
+    }
+    @GetMapping
+    public List<MembresiaDTO> listar(){
+        return dS.list().stream().map(x->{
+            ModelMapper m=new ModelMapper();
+            return m.map(x,MembresiaDTO.class);
+        }).collect(Collectors.toList());
+    }
+}
