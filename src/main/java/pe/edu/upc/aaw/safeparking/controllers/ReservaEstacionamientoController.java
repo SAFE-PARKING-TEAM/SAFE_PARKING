cbb66f55f6baf2bd9c7df42514bc12ca26a41995
DSR124 <246810diegosr@gmail.com>
2023-09-16 16:23:15 -0500

    implementacion de query cantidad de reservas por usuario conductor

--- src/main/java/pe/edu/upc/aaw/safeparking/controllers/ReservaEstacionamientoController.java
+++ src/main/java/pe/edu/upc/aaw/safeparking/controllers/ReservaEstacionamientoController.java
@@ -3,11 +3,13 @@ package pe.edu.upc.aaw.safeparking.controllers;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.aaw.safeparking.dtos.CantidadReservasPorUsuarioDTO;
 import pe.edu.upc.aaw.safeparking.dtos.ReservaEstacionamientoDTO;
 import pe.edu.upc.aaw.safeparking.entities.ReservaEstacionamiento;
 import pe.edu.upc.aaw.safeparking.serviceinterfaces.IReservaEstacionamientoService;
 
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -41,6 +43,24 @@ public class ReservaEstacionamientoController {
     public void eliminar(@PathVariable("id")Integer id){
         reS.delete(id);
     }
+
+    @GetMapping("/query : cantidad de resreva por usuario conductor")
+    public List<CantidadReservasPorUsuarioDTO> cantidadReservasPorUsuario(){
+        List<String[]> lista = reS.quantityreservationByUser();
+        List<CantidadReservasPorUsuarioDTO> listaDTO = new ArrayList<>();
+        for(String[] data:lista){
+
+            CantidadReservasPorUsuarioDTO dto = new CantidadReservasPorUsuarioDTO();
+            dto.setNameUser(data[0]);
+            dto.setNameRole(data[1]);
+            dto.setQuantityReservation(Integer.parseInt(data[2]));
+
+            listaDTO.add(dto);
+        }
+        return listaDTO;
+    }
+
+
 
 
 }
