7fbf84375baec8312fa6d2ec1df7c30224bb78c2
Jhon Harold Sikos Astete <haroldsikos@gmail.com>
2023-09-22 23:35:42 -0500

    Adiciones de permisos en Pago Controller

--- src/main/java/pe/edu/upc/aaw/safeparking/controllers/PagoController.java
+++ src/main/java/pe/edu/upc/aaw/safeparking/controllers/PagoController.java
@@ -31,7 +31,6 @@ public class PagoController {
             return m.map(x,PagoDTO.class);
         }).collect(Collectors.toList());
     }
-
     @DeleteMapping("Eliminar/{id}")
     @PreAuthorize("hasAuthority('administrador')")
     public void eliminar(@PathVariable("id")Integer id){
