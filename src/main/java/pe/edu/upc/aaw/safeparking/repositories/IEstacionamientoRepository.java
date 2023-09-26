de49057d3e4058b2efa5c724a7d52fd64213feab
hectorhorna <u202113453@upc.edu.pe>
2023-09-25 23:08:38 -0500

    Actualización de datos

--- src/main/java/pe/edu/upc/aaw/safeparking/repositories/IEstacionamientoRepository.java
+++ src/main/java/pe/edu/upc/aaw/safeparking/repositories/IEstacionamientoRepository.java
@@ -3,7 +3,7 @@ package pe.edu.upc.aaw.safeparking.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.aaw.safeparking.entities.Estacionamiento;
-import pe.edu.upc.aaw.safeparking.entities.Users;
+import pe.edu.upc.aaw.safeparking.entities.Usuario;
 
 
 @Repository
