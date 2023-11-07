203c24e943c7496565f00113b9668d13562d8cd4
DSR124 <246810diegosr@gmail.com>
2023-11-07 18:33:33 -0500

    cambios 03

--- src/main/java/pe/edu/upc/aaw/safeparking/dtos/UsuarioDTO.java
+++ src/main/java/pe/edu/upc/aaw/safeparking/dtos/UsuarioDTO.java
@@ -124,22 +124,5 @@ public class UsuarioDTO {
         this.enabled = enabled;
     }
 
-    public UsuarioDTO() {
-    }
-
-    public UsuarioDTO(int idUsuario, String nombre, String apellido, String correo, String username, String password, String genero, int dni, String imagen, LocalDate fechaNacimiento, int telefono, MembresiaDTO membresia, Boolean enabled) {
-        this.idUsuario = idUsuario;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.correo = correo;
-        this.username = username;
-        this.password = password;
-        this.genero = genero;
-        this.dni = dni;
-        this.imagen = imagen;
-        this.fechaNacimiento = fechaNacimiento;
-        this.telefono = telefono;
-        this.membresia = membresia;
-        this.enabled = enabled;
-    }
+    
 }
